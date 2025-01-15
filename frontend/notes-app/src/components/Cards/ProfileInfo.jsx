8b5e0126e1546ea76596c9aa5b47b6a34a0e9271
shivam <shiva147m@gmail.com>
2025-01-15 20:37:03 +0530

    pofile and logout button completed

---
+++ frontend/notes-app/src/components/Cards/ProfileInfo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { getInitials } from '../../utils/helper'
+
+function ProfileInfo({ onLogout }) {
+    return (
+        <div className="flex items-center gap-3">
+            <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
+                {getInitials('John Doe')}
+            </div>
+            <div>
+                <p className="text-sm font-medium">John Doe</p>
+                <button className="text-sm text-slate-700 underline" onclick={onLogout}>Logout</button>
+            </div>
+        </div>
+    )
+}
+
+export default ProfileInfo
--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
 
 
 
 function Home() {
     return (
-        <div>
-            Home
-        </div>
+        <>
+            <Navbar />
+        </>
     )
 }
 
