54303774ba201c78581cc554b3b024c865a2cc52
shivam <shiva147m@gmail.com>
2025-05-10 09:32:49 +0530

    'ready'

--- frontend/notes-app/src/utils/axiosInstance.js
+++ frontend/notes-app/src/utils/axiosInstance.js
@@ -1,8 +1,7 @@
 import axios from "axios"
-import { BASE_URL } from "./constants"
 
 const axiosInstance = axios.create({
-    baseURL: BASE_URL,
+    baseURL: process.env.BASE_URL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
