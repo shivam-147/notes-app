75f5713a5d0135806c07832ca6016c2f280e8485
shivam <shiva147m@gmail.com>
2025-05-10 09:42:03 +0530

    try to upload

--- frontend/notes-app/src/utils/axiosInstance.js
+++ frontend/notes-app/src/utils/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
 const axiosInstance = axios.create({
-    baseURL: process.env.BASE_URL,
+    baseURL: BASE_URL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
